91d8119d7bd8a9b4af89b57e4758fbaf226bad4c
Ashna-v <ashnamaryjohn98@gmail.com>
2023-03-14 13:54:14 +0530

    feat: Added a custom button create projects in compliance agreement to create project

--- one_compliance/one_compliance/doctype/compliance_agreement/compliance_agreement.py
+++ one_compliance/one_compliance/doctype/compliance_agreement/compliance_agreement.py
@@ -1,6 +1,24 @@
 
 import frappe
 from frappe.model.document import Document
+from frappe import _
 
 class ComplianceAgreement(Document):
-	pass
+	@frappe.whitelist()
+	def create_project_from_agreement(self):
+		if self.compliance_category_details:
+			for compliance_category in self.compliance_category_details:
+				if compliance_category.compliance_sub_category:
+					project_template  = frappe.db.get_value('Compliance Sub Category',compliance_category.compliance_sub_category,'project_template')
+					if project_template:
+						project = frappe.new_doc('Project')
+						project.project_name = self.customer_name + '-' + compliance_category.compliance_sub_category
+						project.project_template = project_template
+						project.customer = self.customer
+						project.save()
+						frappe.msgprint("Project Created",alert = 1)
+					else :
+						frappe.throw(
+						title = _('ALERT !!'),
+						msg = _('Project Template does not exist')
+						)
--- one_compliance/one_compliance/doctype/compliance_category_details/compliance_category_details.py
+++ one_compliance/one_compliance/doctype/compliance_category_details/compliance_category_details.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2023, efeone and contributors
 # For license information, please see license.txt
 
-# import frappe
+#import frappe
 from frappe.model.document import Document
 
 class ComplianceCategoryDetails(Document):
