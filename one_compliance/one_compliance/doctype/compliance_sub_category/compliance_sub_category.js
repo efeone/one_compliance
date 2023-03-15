b8e80cefe751fff6d2f145b6325078cb779f1d1d
Anjana-sajeevan <anjanasajeevs@gmail.com>
2023-03-15 11:14:47 +0530

    feat: Added custom button to create Project Template from Compliance Sub Category

--- one_compliance/one_compliance/doctype/compliance_sub_category/compliance_sub_category.js
+++ one_compliance/one_compliance/doctype/compliance_sub_category/compliance_sub_category.js
@@ -2,7 +2,16 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Compliance Sub Category', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
+    if(!frm.is_new() && !frm.doc.project_template){
+			//custom button to create project template and route to  project template doctype
+     frm.add_custom_button('Create Project Template', () =>{
+       frappe.model.open_mapped_doc({
+         method: 'one_compliance.one_compliance.doctype.compliance_sub_category.compliance_sub_category.create_project_template_custom_button',
+         frm: cur_frm
+       });
+     });
+    }
+	},
 
-	// }
 });
--- one_compliance/one_compliance/doctype/compliance_sub_category/compliance_sub_category.py
+++ one_compliance/one_compliance/doctype/compliance_sub_category/compliance_sub_category.py
@@ -1,8 +1,25 @@
 # Copyright (c) 2023, efeone and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.model.mapper import *
 
 class ComplianceSubCategory(Document):
 	pass
+
+@frappe.whitelist()
+def create_project_template_custom_button(source_name, target_doc = None):
+	''' Method to get project template for custom button using mapdoc '''
+	def set_missing_values(source, target):
+		target.compliance_category= source.compliance_category
+		target.compliance_sub_category = source.name
+	doc = get_mapped_doc(
+		'Compliance Sub Category',
+		source_name,
+		{
+		'Compliance Sub Category': {
+		'doctype': 'Project Template',
+        },
+		}, target_doc, set_missing_values)
+	return doc
