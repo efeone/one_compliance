cf43959ff8c4ec2ac2bcdf90c4876d551802be8d
Keerthana-ck <keerthana98323@gmail.com>
2023-03-16 17:11:56 +0530

    feat : Added Custom Button to view Compliance Sub category from Compliance Category Doctype

--- one_compliance/one_compliance/doctype/compliance_category/compliance_category.js
+++ one_compliance/one_compliance/doctype/compliance_category/compliance_category.js
@@ -2,7 +2,15 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Compliance Category', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+    if(!frm.is_new() && frm.doc.compliance_category){
+      //custom button to view compliance sub category from compliance category//
+      frm.add_custom_button('Add Sub Category', ()=>{
+        frappe.model.open_mapped_doc({
+          method : 'one_compliance.one_compliance.doctype.compliance_category.compliance_category.custom_button_for_sub_category',
+          frm : cur_frm
+        });
+      });
+    }
+ },
 });
--- one_compliance/one_compliance/doctype/compliance_category/compliance_category.py
+++ one_compliance/one_compliance/doctype/compliance_category/compliance_category.py
@@ -1,8 +1,24 @@
 # Copyright (c) 2023, efeone and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.model.mapper import get_mapped_doc
 
 class ComplianceCategory(Document):
 	pass
+
+@frappe.whitelist()
+def custom_button_for_sub_category(source_name, target_doc = None):
+	''' Method to get Compliance Sub category for custom button using mapdoc '''
+	def set_missing_values(source, target):
+		target.compliance_category = source.compliance_category
+	doc = get_mapped_doc(
+		'Compliance Category',
+		source_name,
+		{
+		'Compliance Category':{
+		'doctype' : 'Compliance Sub Category',
+		},
+		},target_doc, set_missing_values)
+	return doc
