dfb47b80f35440760b165971f93c2cd81d1820c2
Anjana-sajeevan <anjanasajeevs@gmail.com>
2023-03-18 14:46:01 +0530

    feat: Created new doctype Compliance Task Assignment

---
+++ one_compliance/one_compliance/doctype/compliance_task_assignment/compliance_task_assignment.js
@@ -0,0 +1,12 @@
+// Copyright (c) 2023, efeone and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Compliance Task Assignment', {
+	refresh: function(frm) {
+    if(frm.is_new()){
+      frm.add_custom_button('Assign', () =>{
+          
+      })
+    }
+	}
+});
