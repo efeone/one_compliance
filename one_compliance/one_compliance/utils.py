76e00edd4ee88b42781befbd29acce37a02131af
Keerthana-ck <keerthana98323@gmail.com>
2023-03-20 16:05:48 +0530

    feat : Added New Fields in Compliance Sub category Doctype for Email Notification

--- one_compliance/one_compliance/utils.py
+++ one_compliance/one_compliance/utils.py
@@ -1,9 +1,22 @@
 import frappe
 from frappe.utils import *
 
+@frappe.whitelist()
+def create_notification_log(subject, type, for_user, email_content, document_type, document_name):
+    ''' Method to Create Notification Log '''
+    notification_doc = frappe.new_doc('Notification Log')
+    notification_doc.subject = subject
+    notification_doc.type = type
+    notification_doc.for_user = for_user
+    notification_doc.email_content = email_content
+    notification_doc.document_type = document_type
+    notification_doc.document_name = document_name
+    notification_doc.save()
+    frappe.db.commit()
 
 @frappe.whitelist()
 def create_todo(doctype, name, assign_to, owner, description):
+    ''' Method used for create ToDo '''
     todo = frappe.new_doc('ToDo')
     todo.status = 'Open'
     todo.owner = owner
