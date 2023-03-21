4f280e286ed64243021449a8a95fde33b6217fe7
Ashna-v <ashnamaryjohn98@gmail.com>
2023-03-21 12:56:42 +0530

    feat: Optimized inward and outward register

--- one_compliance/one_compliance/doctype/inward_register/inward_register.js
+++ one_compliance/one_compliance/doctype/inward_register/inward_register.js
@@ -7,7 +7,7 @@ frappe.ui.form.on('Inward Register', {
 			// set received by field as session user
       frm.set_value("received_by",frappe.session.user)
     }
-		if(!frm.is_new()){
+		if(!frm.is_new() && frm.doc.docstatus == 1){
 			//custom button to create outward register and route to outward register doctype
 			frm.add_custom_button('Create Outward Register', () =>{
 				frappe.model.open_mapped_doc({
@@ -17,7 +17,7 @@ frappe.ui.form.on('Inward Register', {
 			})
 		}
 		let roles = frappe.user_roles;
-		if(roles.includes("Compliance Manager")){
+		if(roles.includes("Compliance Manager") || roles.includes('Director')){
 			// Set edit posting date and time checkbox visible for compliance manager
 			frm.set_df_property('edit_posting_date_and_time','hidden',0);
 		}
