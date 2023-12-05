2631697f3fcc4a1fb1b1759481c72cc717d1fcb1
Jishna-tp <jishnatp7@gmail.com>
2023-12-05 12:36:26 +0530

    feat: added fields in engagement letter

--- one_compliance/one_compliance/doctype/engagement_letter/engagement_letter.js
+++ one_compliance/one_compliance/doctype/engagement_letter/engagement_letter.js
@@ -2,41 +2,64 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Engagement Letter', {
-	// refresh: function(frm) {
+	setup: function(frm) {
+        // Fetch current date and time
+        var currentDateTime = frappe.datetime.now_datetime();
 
-	// }
-	customer_type: function(frm) {
-		console.log("Customer Type:", frm.doc.customer_type);
-		console.log("Lead:", frm.doc.lead);
-		console.log("Opportunity:", frm.doc.opportunity);
-		if (frm.doc.customer_type === 'Lead' && frm.doc.lead) {
-			frappe.call({
-				method: 'frappe.client.get_value',
-				args: {
-					doctype: 'Lead',
-					filters: { name: frm.doc.lead },
-					fieldname: 'lead_name'
-				},
-				callback: function(response) {
-					if (response.message) {
-						frm.set_value('full_name', response.message.lead_name);
-					}
-				}
-			});
-		} else if (frm.doc.customer_type === 'Opportunity' && frm.doc.opportunity) {
-			frappe.call({
-				method: 'frappe.client.get_value',
-				args: {
-					doctype: 'Opportunity',
-					filters: { name: frm.doc.opportunity },
-					fieldname: 'contact_person'
-				},
-				callback: function(response) {
-					if (response.message) {
-						frm.set_value('full_name', response.message.contact_person);
-					}
-				}
-			});
-		}
-	}
+        // Set the fetched date and time to the fields
+        frm.set_value('posting_date', frappe.datetime.get_today());
+  },
+	lead: function(frm) {
+        if (frm.doc.lead) {
+            frappe.call({
+                method: 'frappe.client.get_value',
+                args: {
+									doctype: 'Lead',
+									filters: { name: frm.doc.lead },
+									fieldname: ['lead_name','email_id','mobile_no']
+                },
+                callback: function(r) {
+                    if (r.message) {
+                        frm.set_value('full_name', r.message.lead_name);
+												frm.set_value('email', r.message.email_id);
+												frm.set_value('mobile', r.message.mobile_no);
+                    }
+                }
+            });
+        }
+    },
+    opportunity: function(frm) {
+      if (frm.doc.opportunity) {
+          frappe.call({
+              method: 'frappe.client.get_value',
+              args: {
+								doctype: 'Opportunity',
+								filters: { name: frm.doc.opportunity },
+								fieldname: ['contact_person','contact_email','contact_mobile']
+              },
+              callback: function(r) {
+                  if (r.message) {
+                      frm.set_value('full_name', r.message.contact_person);
+											frm.set_value('email', r.message.contact_email);
+											frm.set_value('mobile', r.message.contact_mobile);
+                  }
+              }
+          });
+      }
+		},
+		terms: function(frm) {
+        if (frm.doc.terms && !frm.doc.description) {
+            frappe.call({
+                method: 'one_compliance.one_compliance.doctype.engagement_letter.engagement_letter.get_terms_description',
+                args: {
+                    terms: frm.doc.terms
+                },
+                callback: function(r) {
+                    if (r.message) {
+                        frm.set_value('description', r.message);
+                    }
+                }
+            });
+        }
+    }
 });
--- one_compliance/one_compliance/doctype/engagement_letter/engagement_letter.py
+++ one_compliance/one_compliance/doctype/engagement_letter/engagement_letter.py
@@ -6,12 +6,7 @@ from frappe.model.document import Document
 
 class EngagementLetter(Document):
     pass
-    # def validate(self):
-    #     if self.customer_type == 'Lead' and self.lead:
-    #         lead = frappe.get_doc('Lead', self.lead)
-    #         if lead:
-    #             self.full_name = lead.lead_name
-    #     elif self.customer_type == 'Opportunity' and self.opportunity:
-    #         opportunity = frappe.get_doc('Opportunity', self.opportunity)
-    #         if opportunity:
-    #             self.full_name = opportunity.contact_person
+@frappe.whitelist()
+def get_terms_description(terms):
+    description = frappe.db.get_value("Terms and Conditions", terms, "terms")
+    return description
