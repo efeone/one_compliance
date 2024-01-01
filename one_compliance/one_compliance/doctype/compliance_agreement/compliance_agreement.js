c782dc676606b649e8f591629523dc3504869b64
aswanikrishnap <applermon207@gmail.com>
2024-01-01 14:35:47 +0530

    feat:generate invoice and next invoice date

--- one_compliance/one_compliance/doctype/compliance_agreement/compliance_agreement.js
+++ one_compliance/one_compliance/doctype/compliance_agreement/compliance_agreement.js
@@ -25,28 +25,15 @@ frappe.ui.form.on('Compliance Agreement',{
           }
         })
     }
-
-    // Conditional check for creating a sales invoice
-    if (frm.doc.invoice_based_on == 'Consolidated' && frm.doc.invoice_generation && frm.doc.valid_from) {
-        let current_date = frappe.datetime.nowdate();
-        let invoice_date = '';
-
-        if (frm.doc.invoice_generation == 'Monthly') {
-            invoice_date = frappe.datetime.add_months(frm.doc.valid_from, 1);
-        } else if (frm.doc.invoice_generation == 'Quarterly') {
-            invoice_date = frappe.datetime.add_months(frm.doc.valid_from, 3);
-        } else if (frm.doc.invoice_generation == 'Half Yearly') {
-            invoice_date = frappe.datetime.add_months(frm.doc.valid_from, 6);
-        } else if (frm.doc.invoice_generation == 'Yearly') {
-            invoice_date = frappe.datetime.add_years(frm.doc.valid_from, 1);
-        }
-
-        if (invoice_date == current_date) {
-            // Call your function to create the sales invoice here
-            create_sales_invoice(frm, invoice_date);
-        }
-    }
+},
+  invoice_generation: function(frm){
+    get_invoice_dates(frm);
   },
+
+  valid_from: function(frm){
+    get_invoice_dates(frm);
+  },
+
   compliance_category:function(frm){
     update_compliance_category(frm);
   },
@@ -244,3 +231,27 @@ let create_sales_invoice = function(frm, invoice_date) {
         }
   })
 }
+
+
+  const get_invoice_dates = (frm) => {
+    if (frm.doc.invoice_based_on == 'Consolidated' && frm.doc.invoice_generation && frm.doc.valid_from) {
+        let current_date = frappe.datetime.nowdate();
+        let invoice_dates = [];
+
+        if (frm.doc.invoice_generation == 'Monthly') {
+            invoice_dates.push(frappe.datetime.add_months(frm.doc.valid_from, 1));
+            invoice_dates.push(frappe.datetime.add_months(frm.doc.valid_from, 2));
+        } else if (frm.doc.invoice_generation == 'Quarterly') {
+            invoice_dates.push(frappe.datetime.add_months(frm.doc.valid_from, 3));
+            invoice_dates.push(frappe.datetime.add_months(frm.doc.valid_from, 6));
+        } else if (frm.doc.invoice_generation == 'Half Yearly') {
+            invoice_dates.push(frappe.datetime.add_months(frm.doc.valid_from, 6));
+              invoice_dates.push(frappe.datetime.add_months(frm.doc.valid_from, 12));
+        } else if (frm.doc.invoice_generation == 'Yearly') {
+            invoice_dates.push(frappe.datetime.add_years(frm.doc.valid_from, 1));
+            invoice_dates.push(frappe.datetime.add_years(frm.doc.valid_from, 2));
+        }
+        frm.set_value('invoice_date',invoice_dates[0]);
+        frm.set_value('next_invoice_date',invoice_dates[1]);
+    }
+  }
