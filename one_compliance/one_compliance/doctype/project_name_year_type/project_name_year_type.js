d2006dd6fa47f5b11cc054a5f1f1b56c1db2f3e2
aswanikrishnap <applermon207@gmail.com>
2024-01-18 16:48:44 +0530

    feat:fetch from date and to date from reference docname

--- one_compliance/one_compliance/doctype/project_name_year_type/project_name_year_type.js
+++ one_compliance/one_compliance/doctype/project_name_year_type/project_name_year_type.js
@@ -2,7 +2,23 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Project Name Year Type', {
-	// refresh: function(frm) {
-
-	// }
+	
+reference_docname: function (frm) {
+        frappe.call({
+            method: 'one_compliance.one_compliance.doctype.project_name_year_type.project_name_year_type.get_reference_doctype',
+            args: {
+                reference_doctype: frm.doc.reference_doctype,
+                reference_docname: frm.doc.reference_docname
+            },
+            callback: function (r) {
+                if (r.message) {
+                    // Access the fetched document using r.message
+                    var doc = r.message;
+                    // Perform actions based on the fetched document
+                    frm.set_value('from_date', doc.from_date);
+                    frm.set_value('to_date', doc.to_date);
+                }
+            }
+        });
+    }
 });
