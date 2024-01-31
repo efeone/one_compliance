42a8f1b5035117aa271ba6b3bb886dcda3d996bd
Keerthana-ck <keerthana98323@gmail.com>
2024-01-31 09:57:12 +0530

    feat : Updated Project Name

--- one_compliance/one_compliance/doctype/assessment_year/assessment_year.py
+++ one_compliance/one_compliance/doctype/assessment_year/assessment_year.py
@@ -1,8 +1,14 @@
 # Copyright (c) 2024, efeone and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class AssessmentYear(Document):
-	pass
+    def autoname(self):
+        self.name = self.generate_assessment_year_name()
+
+    def generate_assessment_year_name(self):
+        start_year = frappe.utils.getdate(self.year_start_date).year
+        end_year = frappe.utils.getdate(self.year_end_date).year
+        return str(start_year) + '-' + str(end_year)
