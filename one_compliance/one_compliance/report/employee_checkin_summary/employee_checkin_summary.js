24e4a04eee82c3e70ed6ae89be6f6f8ae3d65c73
Jumana-K <jumanahaseen555@gmail.com>
2024-03-04 22:20:55 +0530

    fix:Made Department filter dynamic

--- one_compliance/one_compliance/report/employee_checkin_summary/employee_checkin_summary.js
+++ one_compliance/one_compliance/report/employee_checkin_summary/employee_checkin_summary.js
@@ -19,9 +19,13 @@ frappe.query_reports["Employee Checkin Summary"] = {
 	  {
 		"fieldname": "department",
 		"label": __("Department"),
-		"fieldtype": "Select",
-		"options": "\nGST - C\nIncome Tax - C\nROC - C\nConsulting - C",
-	  },
-	],
+		"fieldtype": "Link",
+		"options": "Department",
+		"get_query": function () {
+			return {
+				"doctype": "Department",
+			};
+		}
+	  }	],
   };
   
