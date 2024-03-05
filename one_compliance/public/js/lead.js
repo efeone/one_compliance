03f460e486a7c3bb6bd1749f77a7a895fbdb01e6
Jishna-tp <jishnatp7@gmail.com>
2024-03-05 13:09:07 +0530

    feat:Create sales invoice from Lead

--- one_compliance/public/js/lead.js
+++ one_compliance/public/js/lead.js
@@ -5,6 +5,42 @@ frappe.ui.form.on('Lead',{
                 frm.remove_custom_button('Customer','Create')
                 frm.remove_custom_button('Quotation','Create')
                 })
+            frm.add_custom_button(__('Sales Invoice'), function () {
+                create_sales_invoice(frm);
+            },__("Create"));
           }
       }
   });
+
+let create_sales_invoice = function (frm) {
+  let d = new frappe.ui.Dialog({
+    title: 'Enter details',
+    fields: [
+      {
+          label: __("Services"),
+          fieldname: "services",
+          fieldtype: 'MultiSelectPills',
+          get_data: function (txt) {
+            return frappe.db.get_link_options("Item", txt);
+          },
+      },
+    ],
+    primary_action_label: 'Create',
+    primary_action(values) {
+      console.log(frm.doc.name);
+      frappe.call({
+        method: 'one_compliance.one_compliance.doc_events.lead.create_sales_invoice',
+        args: {
+            doc_name: frm.doc.name,
+            services:values.services,
+        },
+        callback: function (r) {
+          if (r.message) {
+            d.hide();
+          }
+        }
+      });
+    }
+  });
+  d.show();
+}
