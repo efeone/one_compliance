609a44b8b8f834cf9ee8b8963e360c9227e7e2b6
Reshma396 <reshmakunjuty@gmail.com>
2024-04-18 16:41:31 +0530

    feat:Create project

--- one_compliance/one_compliance/doc_events/customer.py
+++ one_compliance/one_compliance/doc_events/customer.py
@@ -238,102 +238,114 @@ def create_task_from_opportunity(doc, method):
                 add_assignment({"doctype": "Task", "name": task.name, "assign_to": [user_id]})
 
 def create_project_from_customer(doc, method):
-	sub_category = frappe.db.get_single_value("Compliance Settings", 'din_kyc_sub_category')
-	compliance_sub_category = frappe.get_doc('Compliance Sub Category', sub_category)
-	company_list = frappe.get_all("Company")
-	company = frappe.get_doc("Company", company_list)
-	project_template  = compliance_sub_category.project_template
-	project_template_doc = frappe.get_doc('Project Template', project_template)
-	head_of_department = frappe.db.get_value('Employee', {'employee':compliance_sub_category.head_of_department}, 'user_id')
-	if project_template:
-		repeat_on = compliance_sub_category.repeat_on
-		project_based_on_prior_phase = compliance_sub_category.project_based_on_prior_phase
-		previous_month_date = add_months(getdate(doc.custom_annual_general_meeting_date), -1)
-		naming_year = getdate(previous_month_date).year if project_based_on_prior_phase else getdate(doc.custom_annual_general_meeting_date).year
-		naming_month = getdate(previous_month_date).strftime("%B") if project_based_on_prior_phase else getdate(doc.custom_annual_general_meeting_date).strftime("%B")
-		if naming_month in ['January', 'February', 'March']:
-			naming_quarter = 'Quarter 1'
-		elif naming_month in ['April', 'May', 'June']:
-			naming_quarter = 'Quarter 2'
-		elif naming_month in ['July', 'August', 'September']:
-			naming_quarter = 'Quarter 3'
-		else:
-			naming_quarter = 'Quarter 4'
-		if repeat_on == "Yearly":
-			naming = naming_year
-		elif repeat_on == "Quarterly":
-			naming = str(naming_year) + ' ' + naming_quarter
-		else:
-			naming = str(naming_year) + ' ' + naming_month
-		project = frappe.new_doc('Project')
-		project.company = company
-		add_compliance_category_in_project_name = frappe.db.get_single_value('Compliance Settings', 'add_compliance_category_in_project_name')
-		if add_compliance_category_in_project_name:
-			project.project_name = doc.customer_name + '-' + compliance_sub_category.name + '-' + str(naming)
-		else:
-			project.project_name = doc.customer_name + '-' + 'DSC' + '-' +compliance_sub_category.sub_category + '-' + str(naming)
-		project.customer = doc.customer_name
-		project.compliance_sub_category = compliance_sub_category.name
-		project.compliance_category = compliance_sub_category.compliance_category
-		project.expected_start_date = doc.custom_annual_general_meeting_date
-		project.priority = 'Low'
-		project.custom_project_service = compliance_sub_category.name + '-' + str(naming)
-		project.category_type = compliance_sub_category.category_type
-		project.department = compliance_sub_category.department
-		project.save(ignore_permissions=True)
-		if project.compliance_sub_category:
-			if compliance_sub_category and compliance_sub_category.head_of_department:
-				todo = frappe.new_doc('ToDo')
-				todo.status = 'Open'
-				todo.allocated_to = head_of_department
-				todo.description = "project  Assign to " + head_of_department
-				todo.reference_type = 'Project'
-				todo.reference_name = project.name
-				todo.assigned_by = frappe.session.user
-				todo.save(ignore_permissions=True)
-				if todo:
-					frappe.msgprint(("Project is assigned to {0}".format(head_of_department)),alert = 1)
-		frappe.db.commit()
-		frappe.msgprint('Project Created for {0}.'.format(compliance_sub_category.name), alert = 1)
-		for template_task in reversed(project_template_doc.tasks):
-			''' Method to create task against created project from the Project Template '''
-			template_task_doc = frappe.get_doc('Task', template_task.task)
-			user_name = frappe.get_cached_value("User", frappe.session.user, "full_name")
-			task_doc = frappe.new_doc('Task')
-			task_doc.compliance_sub_category = compliance_sub_category.name
-			task_doc.subject = template_task.subject
-			task_doc.project = project.name
-			task_doc.project_name = project.project_name
-			task_doc.category_type = project.category_type
-			task_doc.exp_start_date = doc.custom_annual_general_meeting_date
-			task_doc.custom_serial_number = template_task.idx
-			task_doc.department = compliance_sub_category.department
-			if template_task_doc.expected_time:
-				task_doc.expected_time = template_task_doc.expected_time
-			if template_task.custom_task_duration:
-				task_doc.duration = template_task.custom_task_duration
-				task_doc.exp_end_date = exp_end_date
-			if template_task_doc.depends_on:
-				for depends_task in template_task_doc.depends_on:
-					dependent_task = frappe.get_doc('Task', {'project':project.name,'subject':depends_task.subject}, 'name')
-					task_doc.append("depends_on", {
-                        "task": dependent_task.name,
-                    })
-			task_doc.save(ignore_permissions=True)
-			if template_task.type and template_task.employee_or_group:
-				frappe.db.set_value('Task', task_doc.name, 'assigned_to', template_task.employee_or_group)
-				if template_task.type == "Employee":
-					employee = frappe.db.get_value('Employee', template_task.employee_or_group, 'user_id')
-					if employee and employee != head_of_department:
-						create_todo('Task', task_doc.name, employee, employee, 'Task {0} Assigned Successfully'.format(task_doc.name))
-						create_notification_log('{0} Assigned a New Task {1} to You'.format(user_name, task_doc.name),'Mention', employee, 'Task {0} Assigned Successfully'.format(task_doc.name), task_doc.doctype, task_doc.name)
-				if template_task.type == "Employee Group":
-					employee_group = frappe.get_doc('Employee Group', template_task.employee_or_group)
-					if employee_group.employee_list:
-						for employee in employee_group.employee_list:
-							create_todo('Task', task_doc.name, employee.user_id, employee.user_id, 'Task {0} Assigned Successfully'.format(task_doc.name))
-							create_notification_log('{0} Assigned a New Task {1} to you'.format(user_name, task_doc.name),'Mention', employee.user_id, 'Task {0} Assigned Successfully'.format(task_doc.name), task_doc.doctype, task_doc.name)
+    sub_category = frappe.db.get_single_value("Compliance Settings", 'din_kyc_sub_category')
+    compliance_sub_category = frappe.get_doc('Compliance Sub Category', sub_category)
+    customer_type = doc.compliance_customer_type
+    regularisation_process = frappe.get_value('Customer Type', {'name':customer_type}, ['custom_regularisation_process'])
+    if regularisation_process:
+        company_list = frappe.get_all("Company")
+        company = frappe.get_doc("Company", company_list)
+        project_template  = compliance_sub_category.project_template
+        project_template_doc = frappe.get_doc('Project Template', project_template)
+        head_of_department = frappe.db.get_value('Employee', {'employee':compliance_sub_category.head_of_department}, 'user_id')
+        if project_template:
+            repeat_on = compliance_sub_category.repeat_on
+            project_based_on_prior_phase = compliance_sub_category.project_based_on_prior_phase
+            previous_month_date = add_months(getdate(doc.custom_annual_general_meeting_date), -1)
+            naming_year = getdate(previous_month_date).year if project_based_on_prior_phase else getdate(doc.custom_annual_general_meeting_date).year
+            naming_month = getdate(previous_month_date).strftime("%B") if project_based_on_prior_phase else getdate(doc.custom_annual_general_meeting_date).strftime("%B")
+            if naming_month in ['January', 'February', 'March']:
+                naming_quarter = 'Quarter 1'
+            elif naming_month in ['April', 'May', 'June']:
+                naming_quarter = 'Quarter 2'
+            elif naming_month in ['July', 'August', 'September']:
+                naming_quarter = 'Quarter 3'
+            else:
+                naming_quarter = 'Quarter 4'
+            if repeat_on == "Yearly":
+                naming = naming_year
+            elif repeat_on == "Quarterly":
+                naming = str(naming_year) + ' ' + naming_quarter
+            else:
+                naming = str(naming_year) + ' ' + naming_month
+            for legal_authority in doc.custom_legal_authority_list:
+                if legal_authority.is_regularised == 0:
+                    create_a_director_from_legal_authority(legal_authority.legal_authority, custom_annual_general_meeting_date)
+                    project = frappe.new_doc('Project')
+                    project.company = company
+                    add_compliance_category_in_project_name = frappe.db.get_single_value('Compliance Settings', 'add_compliance_category_in_project_name')
+                    if add_compliance_category_in_project_name:
+                        project.project_name = doc.customer_name + '-' + compliance_sub_category.name + '-' + str(naming)
+                    else:
+                        project.project_name = doc.customer_name + '-' + 'DIN' + '-' +compliance_sub_category.sub_category + '-' + str(naming)
 
-			frappe.db.commit()
-	else:
-		frappe.throw( title = _('ALERT !!'), msg = _('Project Template does not exist for {0}'.format(compliance_sub_category)))
+                    project.project_name = project.project_name+ '-' + legal_authority.legal_authority
+                    project.customer = doc.customer_name
+                    project.compliance_sub_category = compliance_sub_category.name
+                    project.compliance_category = compliance_sub_category.compliance_category
+                    project.expected_start_date = doc.custom_annual_general_meeting_date
+                    project.priority = 'Low'
+                    project.custom_project_service = compliance_sub_category.name + '-' + str(naming)
+                    project.category_type = compliance_sub_category.category_type
+                    project.department = compliance_sub_category.department
+                    project.save(ignore_permissions=True)
+                    if project.compliance_sub_category:
+                        if compliance_sub_category and compliance_sub_category.head_of_department:
+                            todo = frappe.new_doc('ToDo')
+                            todo.status = 'Open'
+                            todo.allocated_to = head_of_department
+                            todo.description = "project  Assign to " + head_of_department
+                            todo.reference_type = 'Project'
+                            todo.reference_name = project.name
+                            todo.assigned_by = frappe.session.user
+                            todo.save(ignore_permissions=True)
+                            if todo:
+                                frappe.msgprint(("Project is assigned to {0}".format(head_of_department)),alert = 1)
+                    frappe.db.commit()
+                    frappe.msgprint('Project Created for {0}.'.format(compliance_sub_category.name), alert = 1)
+                    for template_task in reversed(project_template_doc.tasks):
+                        ''' Method to create task against created project from the Project Template '''
+                        template_task_doc = frappe.get_doc('Task', template_task.task)
+                        user_name = frappe.get_cached_value("User", frappe.session.user, "full_name")
+                        task_doc = frappe.new_doc('Task')
+                        task_doc.compliance_sub_category = compliance_sub_category.name
+                        task_doc.subject = template_task.subject
+                        task_doc.project = project.name
+                        task_doc.project_name = project.project_name
+                        task_doc.category_type = project.category_type
+                        task_doc.exp_start_date = doc.custom_annual_general_meeting_date
+                        task_doc.custom_serial_number = template_task.idx
+                        task_doc.department = compliance_sub_category.department
+                        if template_task_doc.expected_time:
+                            task_doc.expected_time = template_task_doc.expected_time
+                        if template_task.custom_task_duration:
+                            task_doc.duration = template_task.custom_task_duration
+                            task_doc.exp_end_date = exp_end_date
+                        if template_task_doc.depends_on:
+                            for depends_task in template_task_doc.depends_on:
+                                dependent_task = frappe.get_doc('Task', {'project':project.name,'subject':depends_task.subject}, 'name')
+                                task_doc.append("depends_on", {
+                                    "task": dependent_task.name,
+                                })
+                        task_doc.save(ignore_permissions=True)
+                        if template_task.type and template_task.employee_or_group:
+                            frappe.db.set_value('Task', task_doc.name, 'assigned_to', template_task.employee_or_group)
+                            if template_task.type == "Employee":
+                                employee = frappe.db.get_value('Employee', template_task.employee_or_group, 'user_id')
+                                if employee and employee != head_of_department:
+                                    create_todo('Task', task_doc.name, employee, employee, 'Task {0} Assigned Successfully'.format(task_doc.name))
+                                    create_notification_log('{0} Assigned a New Task {1} to You'.format(user_name, task_doc.name),'Mention', employee, 'Task {0} Assigned Successfully'.format(task_doc.name), task_doc.doctype, task_doc.name)
+                            if template_task.type == "Employee Group":
+                                employee_group = frappe.get_doc('Employee Group', template_task.employee_or_group)
+                                if employee_group.employee_list:
+                                    for employee in employee_group.employee_list:
+                                        create_todo('Task', task_doc.name, employee.user_id, employee.user_id, 'Task {0} Assigned Successfully'.format(task_doc.name))
+                                        create_notification_log('{0} Assigned a New Task {1} to you'.format(user_name, task_doc.name),'Mention', employee.user_id, 'Task {0} Assigned Successfully'.format(task_doc.name), task_doc.doctype, task_doc.name)
+
+                        frappe.db.commit()
+        else:
+            frappe.throw( title = _('ALERT !!'), msg = _('Project Template does not exist for {0}'.format(compliance_sub_category)))
+
+@frappe.whitelist()
+def create_a_director_from_legal_authority(legal_authority, annual_general_meeting_date):
+    if custom_annual_general_meeting_date == today:
