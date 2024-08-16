06861188616659c4a1de8bb38fe000154ec7a6a5
Neha Fathima <nehaputhur443@gmail.com>
2024-08-16 10:01:51 +0530

    feat: Create a Button Tds Booking in Sales Invoice

--- one_compliance/one_compliance/doc_events/sales_invoice.py
+++ one_compliance/one_compliance/doc_events/sales_invoice.py
@@ -7,7 +7,6 @@ def sales_invoice_on_submit(doc, method):
         frappe.db.set_value('Project', doc.project, 'status', 'Invoiced')
         frappe.db.set_value('Project', doc.project, 'is_invoiced', 1)
         frappe.db.commit()
-        
     sales_order = frappe.db.get_value("Sales Invoice Item", {"parent":doc.name}, "sales_order")
     frappe.db.set_value("Sales Order", sales_order, "workflow_state", "Invoiced")
 
@@ -22,3 +21,36 @@ def autoname(doc, method=None):
             doc.name = frappe.model.naming.make_autoname(invoice_series_name)
         else:
             frappe.throw(_("Naming Series is not created"))
+
+@frappe.whitelist()
+def create_tds_journal_entry(sales_invoice, customer, tds_account, tds_amount):
+    sales_invoice_doc = frappe.get_doc('Sales Invoice', sales_invoice)
+    tds_amount = float(tds_amount)
+    if tds_amount <= 0:
+        frappe.throw("TDS Amount must be greater than zero.")
+    je = frappe.new_doc('Journal Entry')
+    je.voucher_type = 'Journal Entry'
+    je.posting_date = sales_invoice_doc.posting_date
+    je.company = sales_invoice_doc.company
+    je.remark = f'TDS Booking for Sales Invoice {sales_invoice}'
+    je.append('accounts', {
+        'account': sales_invoice_doc.debit_to,
+        'party_type': 'Customer',
+        'party': customer,
+        'debit_in_account_currency': 0.0,
+        'credit_in_account_currency': tds_amount,
+        'reference_type': 'Sales Invoice',
+        'reference_name': sales_invoice
+    })
+    je.append('accounts', {
+        'account': tds_account,
+        'debit_in_account_currency': tds_amount,
+        'credit_in_account_currency': 0.0
+    })
+    je.save()
+    sales_invoice_doc.paid_amount = tds_amount
+    sales_invoice_doc.outstanding_amount = sales_invoice_doc.rounded_total - tds_amount
+    sales_invoice_doc.flags.ignore_validate_update_after_submit = True
+    sales_invoice_doc.save(ignore_permissions=True)
+    frappe.db.commit()
+    return je.name
