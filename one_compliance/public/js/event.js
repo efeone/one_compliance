294d07acfdaf974f231eafd722df6009a8d8b564
Sherin KR <sherinkrply@gmail.com>
2024-09-21 15:11:48 +0530

    feat: Only active employee need to available to add from employee list on Event creation

--- one_compliance/public/js/event.js
+++ one_compliance/public/js/event.js
@@ -1,9 +1,9 @@
-frappe.ui.form.on('Event',{
-    refresh: function(frm) {
-        frm.add_custom_button(__('Add Employees'), function() {
+frappe.ui.form.on('Event', {
+    refresh: function (frm) {
+        frm.add_custom_button(__('Add Employees'), function () {
             frappe.call({
                 method: 'one_compliance.one_compliance.utils.get_employee_list_for_hod',
-                callback: function(r) {
+                callback: function (r) {
                     if (r.message) {
                         let employees = r.message;
 
@@ -39,16 +39,16 @@ frappe.ui.form.on('Event',{
                         });
 
                         // Handle Search Input
-                        d.$wrapper.find('#employee_search').on('input', frappe.utils.debounce(function() {
+                        d.$wrapper.find('#employee_search').on('input', frappe.utils.debounce(function () {
                             let searchValue = $(this).val().toLowerCase();
-                            d.$wrapper.find('#employee_list li').each(function() {
+                            d.$wrapper.find('#employee_list li').each(function () {
                                 let employeeText = $(this).text().toLowerCase();
                                 $(this).toggle(employeeText.indexOf(searchValue) > -1);
                             });
                         }, 300));  // Added debounce for better performance
 
                         // Handle Employee Selection
-                        d.$wrapper.find('li').on('click', function() {
+                        d.$wrapper.find('li').on('click', function () {
                             let $li = $(this);
                             let employee_id = $li.data('employee-id');
                             let employee_name = $li.data('employee-name');
@@ -80,44 +80,37 @@ frappe.ui.form.on('Event',{
             });
         });
 
-        frm.add_custom_button(
-            __("Create Proforma Invoice"),
-            function () {
-                if (frm.doc.status === 'Completed') {
-                    if (frm.doc.custom_is_billable == 1) {
-                      if(frm.doc.custom_service_description){
-                        if(frm.doc.custom_customer){
-                          create_sales_order(frm);
-                          frappe.msgprint('Proforma Invoice Created Successfully');
-                        }else{
-                          frappe.msgprint('Please Select the Client');
+        frm.add_custom_button(__("Create Proforma Invoice"), function () {
+            if (frm.doc.status === 'Completed') {
+                if (frm.doc.custom_is_billable == 1) {
+                    if (frm.doc.custom_service_description) {
+                        if (frm.doc.custom_customer) {
+                            create_sales_order(frm);
+                            frappe.msgprint('Proforma Invoice Created Successfully');
+                        } else {
+                            frappe.msgprint('Please Select the Client');
                         }
-                      }else{
+                    } else {
                         frappe.msgprint('Pleas Add the Service Description');
-                      }
-                    }else {
-                        frappe.msgprint('Please Mark this Event as Billable');
                     }
-                }else {
-                    frappe.msgprint('Please mark the event as Completed to proceed.');
-                }
-            }
-        );
-        frm.add_custom_button(
-            __("Employee Timesheet"),
-            function () {
-                if (frm.doc.status == 'Completed' && frm.doc.starts_on && frm.doc.ends_on) {
-                    make_time_sheet_entry(frm);
-                    frappe.msgprint('Timesheet Created Successfully');
                 } else {
-                    frappe.msgprint('Please Ensure the Event is Marked as Completed and the End Date is Added');
+                    frappe.msgprint('Please Mark this Event as Billable');
                 }
+            } else {
+                frappe.msgprint('Please mark the event as Completed to proceed.');
             }
-        );
+        });
+        frm.add_custom_button(__("Employee Timesheet"), function () {
+            if (frm.doc.status == 'Completed' && frm.doc.starts_on && frm.doc.ends_on) {
+                make_time_sheet_entry(frm);
+                frappe.msgprint('Timesheet Created Successfully');
+            } else {
+                frappe.msgprint('Please Ensure the Event is Marked as Completed and the End Date is Added');
+            }
+        });
         frm.remove_custom_button("Add Employees", "Add Participants");
     },
-
-    setup: function(frm) {
+    setup: function (frm) {
         frm.set_query('custom_service', () => {
             return {
                 filters: {
@@ -126,10 +119,9 @@ frappe.ui.form.on('Event',{
             };
         });
     },
-    custom_service: function(frm) {
+    custom_service: function (frm) {
         if (frm.doc.custom_service) {
-            frappe.db.get_value('Compliance Sub Category', frm.doc.custom_service, 'rate')
-            .then(r => {
+            frappe.db.get_value('Compliance Sub Category', frm.doc.custom_service, 'rate').then(r => {
                 if (r.message && r.message.rate) {
                     frm.set_value('custom_rate', r.message.rate);
                     frm.set_df_property('custom_rate', 'read_only', 0);
@@ -140,7 +132,7 @@ frappe.ui.form.on('Event',{
     }
 });
 
-let create_sales_order = function(frm) {
+let create_sales_order = function (frm) {
     frappe.call({
         method: 'one_compliance.one_compliance.doc_events.sales_order.create_sales_order_from_event',
         args: {
@@ -150,7 +142,7 @@ let create_sales_order = function(frm) {
             'rate': frm.doc.custom_rate,
             'description': frm.doc.custom_service_description
         },
-        callback: function(r) {
+        callback: function (r) {
             if (r.message) {
                 frm.reload_doc();
             }
@@ -158,13 +150,13 @@ let create_sales_order = function(frm) {
     });
 };
 
-let make_time_sheet_entry = function(frm) {
+let make_time_sheet_entry = function (frm) {
     frappe.call({
         method: 'one_compliance.one_compliance.utils.make_time_sheet_entry',
         args: {
             'event': frm.doc.name
         },
-        callback: function(r) {
+        callback: function (r) {
             if (r.message) {
                 frm.reload_doc();
             }
@@ -173,7 +165,7 @@ let make_time_sheet_entry = function(frm) {
 };
 
 frappe.ui.form.on('Event Participants', {
-    reference_docname: function(frm, cdt, cdn) {
+    reference_docname: function (frm, cdt, cdn) {
         let row = locals[cdt][cdn];
         if (row.reference_doctype === 'Employee') {
             frappe.db.get_value('Employee', row.reference_docname, 'employee_name', (r) => {
