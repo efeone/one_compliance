1b103cc8e1d3ef3ce0c949895e5b51a1745bfaa9
Muhammed Sinan K T <91651425+MhmdSinanKT@users.noreply.github.com>
2024-10-15 09:46:52 +0530

    refact: Report modified to show tasks and events with billing info

--- one_compliance/one_compliance/report/detailed_task_summary/detailed_task_summary.js
+++ one_compliance/one_compliance/report/detailed_task_summary/detailed_task_summary.js
@@ -1,43 +1,55 @@
-// Copyright (c) 2024, efeone and contributors
+// Copyright (c) 2024, efeone Pvt Ltd and contributors
 // For license information, please see license.txt
 
 frappe.query_reports["Detailed Task Summary"] = {
-	"filters": [
-		{
-            label: __("From Date"),
-            fieldname: "from_date",
-            fieldtype: "Date",
-			default: frappe.datetime.add_months(frappe.datetime.get_today(), -1),
-			reqd: 1
-        },
-		{
-            label: __("To Date"),
-            fieldname: "to_date",
-            fieldtype: "Date",
-			default: frappe.datetime.get_today(),
-			reqd: 1
-        },
-		{
-            label: __("Department"),
-            fieldname: "department",
-            fieldtype: "Link",
-            options: "Department",
-			get_query: function(){
-					return {
-						filters: {
-							'is_compliance': 1
-						}
-					}
-				}
-        },
-	],
-	formatter: function (value, row, column, data, default_formatter) {
-		value = default_formatter(value, row, column, data);
-		if (data.department_row) {
-			value = $(`<span>${value}</span>`);
-			var $value = $(value).css("font-weight", "bold");
-			value = $value.wrap("<p></p>").parent().html();
-		}
-		return value;
-	}
+  filters: [
+    {
+      fieldname: "from_date",
+      label: __("From Date"),
+      fieldtype: "Date",
+      default: "Today",
+    },
+    {
+      fieldname: "to_date",
+      label: __("To Date"),
+      fieldtype: "Date",
+      default: "Today",
+    },
+    {
+      fieldname: "client",
+      label: __("Client"),
+      fieldtype: "Link",
+      options: "Customer",
+    },
+    {
+      fieldname: "employee",
+      label: __("Employee"),
+      fieldtype: "Link",
+      options: "Employee",
+    },
+    {
+      fieldname: "status",
+      label: __("Status"),
+      fieldtype: "Select",
+      options: "\nOpen\nWorking\nCompleted",
+    },
+    {
+      fieldname: "reference_type",
+      label: __("Reference Type"),
+      fieldtype: "Select",
+      options: "\nTask\nEvent",
+    },
+    {
+      fieldname: "sub_category",
+      label: __("Sub Category"),
+      fieldtype: "Link",
+      options: "Compliance Sub Category",
+    },
+    {
+      fieldname: "department",
+      label: __("Department"),
+      fieldtype: "Link",
+      options: "Department",
+    },
+  ],
 };
