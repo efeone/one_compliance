2e3f5da36feebf5a133128919891ed0e7c8d3b43
Neha Fathima <nehaputhur443@gmail.com>
2025-09-01 14:29:53 +0530

    feat: update the status submitted after submission of doctype

--- one_compliance/one_compliance/doctype/engagement_letter/engagement_letter.py
+++ one_compliance/one_compliance/doctype/engagement_letter/engagement_letter.py
@@ -5,4 +5,8 @@ from frappe.model.document import Document
 
 
 class EngagementLetter(Document):
-    pass
+    
+
+    def before_submit(self):
+        """Set the Engagement Letter status to 'Submitted'"""
+        self.status = "Submitted"
