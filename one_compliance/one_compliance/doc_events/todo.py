27882a5cc826fe2e5a16f956fda4ed148020e660
ajmalroshan123 <ajmalroshan2002@gmail.com>
2025-09-09 10:15:49 +0530

    refactor: unify company assignment from Task, Project, and Sales Order into single handler

--- one_compliance/one_compliance/doc_events/todo.py
+++ one_compliance/one_compliance/doc_events/todo.py
@@ -1,26 +1,32 @@
 import frappe
 
-def set_company_from_task(doc, method):
-    if doc.reference_type == "Task" and doc.reference_name:
-        company = frappe.db.get_value("Task", doc.reference_name, "company")
+def set_company_and_related_fields(doc, method):
+    """Set company (and related fields) based on reference_type and reference_name."""
+    if not doc.reference_type or not doc.reference_name:
+        return
+
+    ref_type = doc.reference_type
+    ref_name = doc.reference_name
+
+    if ref_type == "Task":
+        company = frappe.db.get_value("Task", ref_name, "company")
         if company:
             doc.company = company
 
-def set_company_from_project(doc, method):
-    if doc.reference_type == "Project" and doc.reference_name:
-        company = frappe.db.get_value("Project", doc.reference_name, "company")
+    elif ref_type == "Project":
+        company = frappe.db.get_value("Project", ref_name, "company")
         if company:
             doc.company = company
 
-def set_company_from_event(doc, method):
-    if doc.reference_type == "Sales Order" and doc.reference_name:
-        company = frappe.db.get_value("Sales Order", doc.reference_name, "company")
-        client = frappe.db.get_value("Sales Order", doc.reference_name, "customer")
+    elif ref_type == "Sales Order":
+        company, client = frappe.db.get_value("Sales Order", ref_name, ["company", "customer"])
         sub_category = frappe.db.get_value(
             "Sales Order Item",
-            {"parent": doc.reference_name},
-            "custom_compliance_subcategory")
+            {"parent": ref_name},
+            "custom_compliance_subcategory"
+        )
         if company:
             doc.company = company
             doc.client = client
             doc.compliance_sub_category = sub_category
+
