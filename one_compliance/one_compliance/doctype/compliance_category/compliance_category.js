f2d8ae4039d51a44395a450ba4898747fcba4dc7
NaseeraVP <naseeravp38@gmail.com>
2025-09-16 15:16:01 +0530

    refactor:simplify and optimize client script

--- one_compliance/one_compliance/doctype/compliance_category/compliance_category.js
+++ one_compliance/one_compliance/doctype/compliance_category/compliance_category.js
@@ -2,71 +2,77 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Compliance Category', {
-  setup:function(frm){
-    set_filters(frm);
-  },
-	refresh: function(frm) {
-    if(!frm.is_new() && frm.doc.compliance_category){
-      //custom button to view compliance sub category from compliance category//
-      frm.add_custom_button('Add Sub Category', ()=>{
-        frappe.model.open_mapped_doc({
-          method : 'one_compliance.one_compliance.doctype.compliance_category.compliance_category.custom_button_for_sub_category',
-          frm : cur_frm
-        });
-      });
-    }
-  },
-  department: function(frm) {
-    if(frm.doc.department){
-      frappe.call({
-        method: 'one_compliance.one_compliance.doctype.compliance_category.compliance_category.fetch_employees',
-        args:{
-          'department':frm.doc.department
-        },
-        callback: (r) =>{
-          if (r.message){
-            let compliance_executives = r.message;
-            if(compliance_executives && compliance_executives.length){
-              frm.clear_table('compliance_executive');
-              compliance_executives.forEach((compliance_executive) => {
-                let row = frm.add_child('compliance_executive', {
-                  employee: compliance_executive.name,
-                  employee_name: compliance_executive.employee_name,
-                  designation: compliance_executive.designation
-                });
-              });
-            }
-            frm.refresh_field('compliance_executive');
-          }
-        }
-      });
-    }
-  }
+	setup: (frm) => {
+		set_filters(frm);
+	},
+	refresh: (frm) => {
+		add_sub_category_button(frm);
+	},
+	department: (frm) => {
+		fetch_and_set_compliance_executives(frm);
+	}
 });
 
-let set_filters = function(frm){
-  // applied filter for getting head of department
-  frm.set_query('head_of_department', function() {
-    return{
-      filters:{
-        'department': frm.doc.department,
-        'designation': 'Head Of Department'
-      }
-    }
-  });
-  // applied filter for designation in child table
-  frm.set_query('employee','compliance_executive', () => {
-    return {
-      filters:{
-        status: 'Active'
-      }
-    }
-  });
-  frm.set_query('department', () => {
-    return {
-      filters:{
-        is_compliance: 1
-      }
-    }
-  });
+/**
+ * Adds a custom button 'Add Sub Category' to create a new Sub Category linked to the current Compliance Category
+ */
+function add_sub_category_button(frm) {
+	if(!frm.is_new() && frm.doc.compliance_category){
+	  frm.add_custom_button('Add Sub Category', ()=>{
+		frappe.model.open_mapped_doc({
+		  method : 'one_compliance.one_compliance.doctype.compliance_category.compliance_category.custom_button_for_sub_category',
+		  source_name: frm.doc.name
+		});
+	  });
+	}
+}
+
+/**
+ * Fetch and set compliance executives based on department
+ */
+function fetch_and_set_compliance_executives(frm) {
+	if (!frm.doc.department) return;
+
+	frappe.call({
+		method: 'one_compliance.one_compliance.doctype.compliance_category.compliance_category.fetch_employees',
+		args: { department: frm.doc.department },
+		callback: r => {
+			const executives = r.message;
+			if (!executives || !executives.length) return;
+
+			frm.clear_table('compliance_executive');
+			executives.forEach(exec => {
+				frm.add_child('compliance_executive', {
+					employee: exec.name,
+					employee_name: exec.employee_name,
+					designation: exec.designation
+				});
+			});
+			frm.refresh_field('compliance_executive');
+		}
+	});
+}
+
+/*
+ * Set filters for fields in Compliance Category form
+ */
+function set_filters(frm) {
+	frm.set_query('head_of_department', () => ({
+		filters: {
+			department: frm.doc.department,
+			designation: 'Head Of Department'
+		}
+	}));
+
+	frm.set_query('employee', 'compliance_executive', () => ({
+		filters: {
+			status: 'Active'
+		}
+	}));
+
+	frm.set_query('department', () => ({
+		filters: {
+			is_compliance: 1
+		}
+	}));
 }
--- one_compliance/one_compliance/doctype/compliance_category/compliance_category.py
+++ one_compliance/one_compliance/doctype/compliance_category/compliance_category.py
@@ -9,22 +9,27 @@ class ComplianceCategory(Document):
 	pass
 
 @frappe.whitelist()
-def custom_button_for_sub_category(source_name, target_doc = None):
-	''' Method to get Compliance Sub category for custom button using mapdoc '''
-	def set_missing_values(source, target):
-		target.compliance_category = source.compliance_category
-	doc = get_mapped_doc(
-		'Compliance Category',
-		source_name,
-		{
-		'Compliance Category':{
-		'doctype' : 'Compliance Sub Category',
-		},
-		},target_doc, set_missing_values)
-	return doc
+def custom_button_for_sub_category(source_name, target_doc=None):
+    """Method to get Compliance Sub Category for custom button using mapdoc."""
 
+    def set_missing_values(source, target):
+        target.compliance_category = source.compliance_category
+
+    doc = get_mapped_doc(
+        "Compliance Category",
+        source_name,
+        {
+            "Compliance Category": {
+                "doctype": "Compliance Sub Category",
+            },
+        },
+        target_doc,
+        set_missing_values,
+    )
+    return doc
 
 @frappe.whitelist()
 def fetch_employees(department):
+	''' Method to fetch employees based on department '''
 	employee_list = frappe.db.get_list('Employee', filters={'department':department}, fields=['name', 'employee_name', 'designation'])
 	return employee_list
