6d66fa630bb2b5404fdad858ee5a6460afb1097b
NaseeraVP <naseeravp38@gmail.com>
2025-10-07 16:19:16 +0530

    refactor: simplify and optimize Project Template client and server scripts (#560)

--- one_compliance/one_compliance/doc_events/project_template.py
+++ one_compliance/one_compliance/doc_events/project_template.py
@@ -1,76 +1,71 @@
 import frappe
+import json
 from frappe.model.document import Document
-from frappe.model.mapper import *
 from frappe import _
-
 
 @frappe.whitelist()
 def update_project_template(doc, method=None):
-    ''' Method to set project template in Compliance Sub Category '''
+    """Set project template in Compliance Sub Category."""
     frappe.db.set_value('Compliance Sub Category', doc.compliance_sub_category, 'project_template', doc.name)
-    frappe.db.commit()
 
 @frappe.whitelist()
 def get_existing_documents(template, task):
+    """Get documents for a task from custom_documents_required table."""
     project_template = frappe.get_doc("Project Template", template)
-    existing_documents = []
-    # Find documents corresponding to the task in custom_documents_required table
     for row in project_template.custom_documents_required:
         if row.task == task:
-            existing_documents = row.documents.split(', ')
-    return existing_documents
+            return row.documents.split(', ')
+    return []
 
 @frappe.whitelist()
-def update_documents_required(template, task, documents = None):
+def update_documents_required(template, task, documents=None):
+    """Update or remove documents required for a task in a project template."""
     project_template = frappe.get_doc("Project Template", template)
-    project_template_task = next((row for row in project_template.tasks if row.task == task), None)
-    existing_row = next((row for row in project_template.custom_documents_required if row.task == task), None)
-    if(documents):
-        document = json.loads(documents)
-        documents_string = ', '.join(document)
+    task_row = next((row for row in project_template.tasks if row.task == task), None)
+    doc_row = next((row for row in project_template.custom_documents_required if row.task == task), None)
 
-        if project_template_task:
-            project_template_task.custom_has_document = 1
-        if existing_row:
-            # Update the existing row
-            existing_row.documents = documents_string
+    if documents:
+        documents_list = json.loads(documents)
+        documents_string = ', '.join(documents_list)
+        if task_row:
+            task_row.custom_has_document = 1
+        if doc_row:
+            doc_row.documents = documents_string
         else:
-            project_template.append("custom_documents_required",{
+            project_template.append("custom_documents_required", {
                 "task": task,
                 "documents": documents_string
             })
-
-        project_template.save()
-        return 'success'
     else:
-        if project_template_task:
-            project_template_task.custom_has_document = 0
-        if existing_row:
-            project_template.custom_documents_required.remove(existing_row)
-            project_template.save()
-            return 'success'
+        if task_row:
+            task_row.custom_has_document = 0
+        if doc_row:
+            project_template.custom_documents_required.remove(doc_row)
+    project_template.save()
+    return 'success'
 
 def on_trash(doc, method):
-    if doc.compliance_sub_category:
-        if frappe.db.exists("Compliance Sub Category", doc.compliance_sub_category):
-            compliance_sub_category = frappe.get_doc("Compliance Sub Category", doc.compliance_sub_category)
-            if compliance_sub_category.project_template == doc.name:
-                compliance_sub_category.set('project_template', None)
-                compliance_sub_category.save()
+    """Clear project_template in Compliance Sub Category if the template is deleted."""
+    sub_category = doc.compliance_sub_category
+    if sub_category and frappe.db.exists("Compliance Sub Category", sub_category):
+        compliance_sub_category = frappe.get_doc("Compliance Sub Category", sub_category)
+        if compliance_sub_category.project_template == doc.name:
+            compliance_sub_category.set('project_template', None)
+            compliance_sub_category.save()
 
 def validate(doc, method):
     """
-        Method checks if project template exists for given compliance sub category
-        Output
-            Method throws error if a project template with the given compliance sub category is found
+    Ensure only one project template exists per compliance sub category.
+    Throws error if duplicate found.
     """
-    if not doc.compliance_sub_category:
+    sub_category = doc.compliance_sub_category
+    if not sub_category:
         return
     existing_template = frappe.db.exists({
         "doctype": "Project Template",
-        "compliance_sub_category": doc.compliance_sub_category,
+        "compliance_sub_category": sub_category,
         "name": ["!=", doc.name]
     })
     if existing_template:
-        sub_category_name = frappe.get_value("Compliance Sub Category", doc.compliance_sub_category, "name")
+        sub_category_name = frappe.get_value("Compliance Sub Category", sub_category, "name")
         frappe.throw(_("Project Template already exists for <b>{0}</b>").format(sub_category_name))
--- one_compliance/public/js/project_template.js
+++ one_compliance/public/js/project_template.js
@@ -1,71 +1,70 @@
 // Copyright (c) 2024, efeone and contributors
 // For license information, please see license.txt
-
 frappe.ui.form.on('Project Template', {
-    setup: function(frm) {
-        set_filters(frm)
-        restrict_task_field(frm);
-        restrict_premium_task_field(frm);
+    setup(frm) {
+        set_filters(frm);
+        restrict_task_field(frm, 'tasks');
+        restrict_task_field(frm, 'premium_tasks');
+    },
+    refresh(frm) {
+        clear_blank_rows(frm, ["tasks", "premium_tasks"]);
     },
     custom_add_tasks(frm) {
-        new_task_popup(frm);
+        show_task_popup(frm, 'tasks');
     },
-    custom_add_tasks2: function(frm) {
-        new_premium_task_popup(frm);
+    custom_add_tasks2(frm) {
+        show_task_popup(frm, 'premium_tasks');
     }
 });
 
 frappe.ui.form.on('Project Template Task', {
-    custom_documents_required: function (frm, cdt, cdn) {
-        var child = locals[cdt][cdn];
-        if(child.custom_has_document){
-            if(frm.is_new()){
-                frappe.throw('You need to save the document to perform this action.')
-            }
-            else {
+    custom_documents_required(frm, cdt, cdn) {
+        const child = locals[cdt][cdn];
+        if (child.custom_has_document) {
+            if (frm.is_new()) {
+                frappe.throw('You need to save the document to perform this action.');
+            } else {
                 frappe.call({
                     method: 'one_compliance.one_compliance.doc_events.project_template.get_existing_documents',
                     args: {
                         template: frm.doc.name,
                         task: child.task,
                     },
-                    callback: function (r) {
+                    callback(r) {
                         if (r.message) {
-                            var documents_required = r.message;
-                            documents_required_popup(frm, documents_required, child);
+                            documents_required_popup(frm, r.message, child);
                         }
                     }
-                });    
+                });
             }
         }
     },
-    custom_has_document: function (frm, cdt, cdn) {
-        var child = locals[cdt][cdn];
-        var customDocumentsTable = frm.doc.custom_documents_required || [];
-        // If custom_has_document is unchecked, remove related documents from custom_documents_required
+    custom_has_document(frm, cdt, cdn) {
+        const child = locals[cdt][cdn];
         if (!child.custom_has_document) {
-            // Filter out rows related to the task
-            frm.doc.custom_documents_required = customDocumentsTable.filter(function(row) {
-                return row.task !== child.task;
-            });
-            frm.refresh_field('custom_documents_required'); // Refresh the field to reflect the changes
+            frm.doc.custom_documents_required = (frm.doc.custom_documents_required || []).filter(
+                row => row.task !== child.task
+            );
+            frm.refresh_field('custom_documents_required');
         }
     }
 });
 
-function set_filters(frm){
-    //filter the compliance sub category based on compliance category
-    frm.set_query('compliance_sub_category', () => {
-        return {
-            filters: {
-                compliance_category : frm.doc.compliance_category
-            }
-        }
-    });
+/**
+ * Sets up filters and restricts row addition for task and premium task tables on the Project Template form.
+ * Called during the form's setup event.
+ */
+function set_filters(frm) {
+    frm.set_query('compliance_sub_category', () => ({
+        filters: { compliance_category: frm.doc.compliance_category }
+    }));
 }
 
-function documents_required_popup(frm, documents_required, child){
-    var dialog = new frappe.ui.Dialog({
+/**
+ * Displays a dialog to select or create documents required for a task.
+ */
+function documents_required_popup(frm, documents_required, child) {
+    const dialog = new frappe.ui.Dialog({
         title: __("Documents Required"),
         fields: [
             {
@@ -73,28 +72,26 @@ function documents_required_popup(frm, documents_required, child){
                 fieldname: "documents_required",
                 fieldtype: 'MultiSelectPills',
                 default: documents_required,
-                get_data: function (txt) {
-                    return frappe.db.get_link_options("Task Document", txt);
-                },
+                get_data: txt => frappe.db.get_link_options("Task Document", txt),
             }
         ],
-        primary_action: function (values) {
-            documents_required_primary_action(frm, values, child);
+        primary_action_label: __("Save"),
+        primary_action(values) {
+            update_documents_required(frm, values, child);
             dialog.hide();
         },
-        primary_action_label: __("Save"),
         secondary_action_label: __("Create a New Task Document"),
-        secondary_action: function() {
-            // Open a new dialog to create a Task Document
-            frappe.new_doc('Task Document', function(doc) {
-
-            });
+        secondary_action() {
+            frappe.new_doc('Task Document');
         }
     });
     dialog.show();
 }
 
-function documents_required_primary_action(frm, values, child){
+/**
+ * Calls the backend to update the documents required for a task.
+ */
+function update_documents_required(frm, values, child) {
     frappe.call({
         method: 'one_compliance.one_compliance.doc_events.project_template.update_documents_required',
         args: {
@@ -102,11 +99,10 @@ function documents_required_primary_action(frm, values, child){
             documents: values.documents_required,
             task: child.task,
         },
-        callback: function(r) {
+        callback(r) {
             if (r.message === 'success') {
                 frm.reload_doc();
-            }
-            else {
+            } else {
                 frappe.msgprint('Error: Unable to update documents required.');
             }
         }
@@ -114,20 +110,28 @@ function documents_required_primary_action(frm, values, child){
 }
 
 /**
- * The functions below are used to show a popup that will add a task into the task table and disable adding rows the native way
- *
+ * Restricts native row addition in a child table and enforces use of the popup dialog.
+ * Also sets the 'task' field to only allow selection from existing tasks.
+ * @param {string} table_field - The name of the child table field (e.g., 'tasks' or 'premium_tasks').
  */
-function restrict_task_field(frm){
-    frm.fields_dict.tasks.grid.update_docfield_property('task', 'only_select', 1);
-    frm.set_df_property('tasks', 'cannot_add_rows', true);
-    frappe.model.get_children(frm.doc, "tasks").forEach(e=>{
-        frappe.meta.get_docfield('Project Template Task', 'task', e.name).only_select = true;
-    })
+function restrict_task_field(frm, table_field) {
+    const grid = frm.fields_dict[table_field].grid;
+    grid.update_docfield_property('task', 'only_select', 1);
+    grid.cannot_add_rows = true;
+    grid.refresh();
+    if (grid.wrapper) {
+        grid.wrapper.find('.grid-add-row').hide();
+    }
 }
 
-function new_task_popup(frm){
+/**
+ * Shows a dialog to add a new task or select an existing one for a given table (tasks or premium_tasks).
+ * Handles both creation and selection logic.
+ * @param {string} table_field - The name of the child table field.
+ */
+function show_task_popup(frm, table_field) {
     let primary_action_label = 'Create & Add';
-    let dialog = new frappe.ui.Dialog({
+    const dialog = new frappe.ui.Dialog({
         title: 'Task details',
         fields: [
             {
@@ -135,14 +139,8 @@ function new_task_popup(frm){
                 fieldname: 'is_existing_task',
                 fieldtype: 'Check',
                 change: () => {
-                    let is_existing_task = dialog.get_value('is_existing_task');
-                    if(is_existing_task){
-                        primary_action_label = 'Add';
-                    }
-                    else {
-                        primary_action_label = 'Create & Add';
-                    }
-                    set_primary_action_label(dialog, primary_action_label)
+                    primary_action_label = dialog.get_value('is_existing_task') ? 'Add' : 'Create & Add';
+                    dialog.set_primary_action_label(primary_action_label);
                 }
             },
             {
@@ -151,25 +149,16 @@ function new_task_popup(frm){
                 fieldtype: 'Link',
                 options: 'Task',
                 only_select: 1,
-                get_query: function(){
-                    return {
-                        filters: {
-                            is_template: 1
-                        }
-                    }
-                },
+                get_query: () => ({ filters: { is_template: 1 } }),
                 depends_on: 'eval: doc.is_existing_task',
                 mandatory_depends_on: 'eval: doc.is_existing_task',
                 change: () => {
-                    let task = dialog.get_value('task');
-                    frappe.db.get_value('Task', task, 'subject').then(r => {
-                        if(r.message.subject){
-                            dialog.set_value('subject', r.message.subject);
-                        }
-                        else {
-                            dialog.set_value('subject', );
-                        }
-                    });
+                    const task = dialog.get_value('task');
+                    if (task) {
+                        frappe.db.get_value('Task', task, 'subject').then(r => {
+                            dialog.set_value('subject', r.message.subject || '');
+                        });
+                    }
                 }
             },
             {
@@ -180,149 +169,61 @@ function new_task_popup(frm){
                 mandatory_depends_on: 'eval: !doc.is_existing_task',
             }
         ],
-        primary_action_label: primary_action_label,
+        primary_action_label,
         primary_action(values) {
-            if(values.is_existing_task){
-                add_task_row(frm, values.task, values.subject)
-            }
-            else {
-                create_task(frm, values.subject);
+            if (values.is_existing_task) {
+                add_task_row(frm, table_field, values.task, values.subject);
+            } else {
+                create_task(frm, table_field, values.subject);
             }
             dialog.hide();
         }
     });
     dialog.show();
-}
-
-function create_task(frm, subject){
-    frappe.db.insert({
-        doctype: 'Task',
-        subject: subject,
-        is_template: 1,
-        status: 'Template'
-    }).then(doc => {
-        if(doc.name){
-            add_task_row(frm, doc.name, subject)
-        }
-    });
-}
-
-function add_task_row(frm, task, subject){
-    frm.add_child('tasks',{
-        'task': task,
-        'subject': subject
-    });
-    frm.refresh_field('tasks');
-    restrict_task_field(frm);
-}
-
-function set_primary_action_label(dialog, primary_action_label) {
-    dialog.get_primary_btn().removeClass("hide").html(primary_action_label);
 }
 
 /**
- * The functions below are used to show a popup that will add a task into the Premium Task table and disable adding rows the native way
- *
+ * Creates a new Task document and adds it as a row to the specified child table.
+ * @param {object} frm - The current form object.
+ * @param {string} table_field - The name of the child table field.
+ * @param {string} subject - The subject/title of the new task.
  */
-function restrict_premium_task_field(frm) {
-    frm.fields_dict.premium_tasks.grid.update_docfield_property('task', 'only_select', 1);
-    frm.fields_dict.premium_tasks.grid.cannot_add_rows = true;
-    frm.fields_dict.premium_tasks.grid.refresh();
-    frm.fields_dict.premium_tasks.grid.wrapper.find('.grid-add-row').hide();
-}
-
-function new_premium_task_popup(frm) {
-    let primary_action_label = 'Create & Add';
-
-    let dialog = new frappe.ui.Dialog({
-        title: 'Task details',
-        fields: [
-            {
-                label: 'Is Existing Task',
-                fieldname: 'is_existing_task',
-                fieldtype: 'Check',
-                change: () => {
-                    let is_existing = dialog.get_value('is_existing_task');
-                    if (is_existing) {
-                        primary_action_label = 'Add';
-                    } else {
-                        primary_action_label = 'Create & Add';
-                    }
-                    set_primary_action_label(dialog, primary_action_label);
-                }
-            },
-            {
-                label: 'Task',
-                fieldname: 'task',
-                fieldtype: 'Link',
-                options: 'Task',
-                only_select: 1,
-                get_query: function() {
-                    return {
-                        filters: {
-                            is_template: 1
-                        }
-                    };
-                },
-                depends_on: 'eval: doc.is_existing_task',
-                mandatory_depends_on: 'eval: doc.is_existing_task',
-                change: () => {
-                    let task = dialog.get_value('task');
-                    if (task) {
-                        frappe.db.get_value('Task', task, 'subject').then(r => {
-                            if (r.message.subject) {
-                                dialog.set_value('subject', r.message.subject);
-                            } else {
-                                dialog.set_value('subject', '');
-                            }
-                        });
-                    }
-                }
-            },
-            {
-                label: 'Subject',
-                fieldname: 'subject',
-                fieldtype: 'Data',
-                depends_on: 'eval: !doc.is_existing_task',
-                mandatory_depends_on: 'eval: !doc.is_existing_task'
-            }
-        ],
-        primary_action_label: primary_action_label,
-        primary_action(values) {
-            if (values.is_existing_task) {
-                add_premium_task_row(frm, values.task, values.subject);
-            } else {
-                create_premium_task(frm, values.subject);
-            }
-            dialog.hide();
-        }
-    });
-
-    dialog.show();
-}
-
-function set_primary_action_label(dialog, label) {
-    dialog.set_primary_action_label(label);
-}
-
-function create_premium_task(frm, subject) {
+function create_task(frm, table_field, subject) {
     frappe.db.insert({
         doctype: 'Task',
-        subject: subject,
+        subject,
         is_template: 1,
         status: 'Template'
     }).then(doc => {
         if (doc.name) {
-            add_premium_task_row(frm, doc.name, subject);
+            add_task_row(frm, table_field, doc.name, subject);
         }
     });
 }
 
-function add_premium_task_row(frm, task, subject) {
-    frm.add_child('premium_tasks', {
-        'task': task,
-        'subject': subject
-    });
-    frm.refresh_field('premium_tasks');
-    restrict_premium_task_field(frm);
+/**
+ * Adds a row to the specified child table with the given task and subject.
+ * Refreshes the table and re-applies restrictions.
+ * @param {string} table_field - The name of the child table field.
+ * @param {string} task - The name of the Task document.
+ * @param {string} subject - The subject/title of the task.
+ */
+function add_task_row(frm, table_field, task, subject) {
+    frm.add_child(table_field, { task, subject });
+    frm.refresh_field(table_field);
+    restrict_task_field(frm, table_field);
+}
+
+/**
+ * Clears unwanted blank rows from specified child tables
+ * if the parent doc is new (unsaved).
+ * @param {Array<string>} tables - List of child table fieldnames.
+ */
+function clear_blank_rows(frm, tables) {
+    if (frm.is_new()) {
+        tables.forEach(table_field => {
+            frm.clear_table(table_field);
+            frm.refresh_field(table_field);
+        });
+    }
 }
