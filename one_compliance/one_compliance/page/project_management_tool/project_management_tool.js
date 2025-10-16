2f19f5abe7171642facb3edc8af223db993b5f0b
hridyalakshmi <125202476+hridyalakshmi@users.noreply.github.com>
2025-10-16 11:15:39 +0530

    fix: Project filter in Task Management Tool (#562)

--- one_compliance/one_compliance/page/project_management_tool/project_management_tool.js
+++ one_compliance/one_compliance/page/project_management_tool/project_management_tool.js
@@ -1,4 +1,4 @@
-frappe.pages['project-management_tool'].on_page_load = function(wrapper) {
+frappe.pages['project-management_tool'].on_page_load = function (wrapper) {
 	var page = frappe.ui.make_app_page({
 		parent: wrapper,
 		title: 'Project Management Tool',
@@ -6,7 +6,7 @@ frappe.pages['project-management_tool'].on_page_load = function(wrapper) {
 	});
 
 	// Button to refresh the page
-	let $button = page.set_secondary_action('Refresh',() => location.reload())
+	let $button = page.set_secondary_action('Refresh', () => location.reload())
 
 	page.main.addClass("frappe-card");
 
@@ -23,31 +23,31 @@ function make_filters(page) {
 		options: "Project",
 		change() {
 			if (page.fields_dict.project.get_value()) {
-          refresh_projects(page);
-      }
+				refresh_projects(page);
+			}
 		}
 	});
-	page.fields_dict.project.$input.on('change', function() {
+	page.fields_dict.project.$input.on('change', function () {
 		if (!page.fields_dict.project.get_value()) {
-				refresh_projects(page);
+			refresh_projects(page);
 		}
-  });
+	});
 	let customerField = page.add_field({
 		label: __("Customer"),
 		fieldname: "customer",
 		fieldtype: "Link",
-		options:"Customer",
+		options: "Customer",
 		change() {
 			if (page.fields_dict.customer.get_value()) {
-          refresh_projects(page);
-      }
+				refresh_projects(page);
+			}
 		}
 	});
-	page.fields_dict.customer.$input.on('change', function() {
+	page.fields_dict.customer.$input.on('change', function () {
 		if (!page.fields_dict.customer.get_value()) {
-				refresh_projects(page);
+			refresh_projects(page);
 		}
-  });
+	});
 	let employeeField = page.add_field({
 		label: __("Employee"),
 		fieldname: "employee",
@@ -56,16 +56,16 @@ function make_filters(page) {
 		default: get_employee_id(),
 		change() {
 			if (page.fields_dict.employee.get_value()) {
-          refresh_projects(page);
-      }
+				refresh_projects(page);
+			}
 		},
 		read_only: frappe.session.user === 'Administrator' ? 0 : 1
 	});
-	page.fields_dict.employee.$input.on('change', function() {
+	page.fields_dict.employee.$input.on('change', function () {
 		if (!page.fields_dict.employee.get_value()) {
-				refresh_projects(page);
+			refresh_projects(page);
 		}
-  });
+	});
 	let categoryField = page.add_field({
 		label: __("Department"),
 		fieldname: "department",
@@ -73,15 +73,15 @@ function make_filters(page) {
 		options: "Department",
 		change() {
 			if (page.fields_dict.department.get_value()) {
-          refresh_projects(page);
-      }
+				refresh_projects(page);
+			}
 		}
 	});
-	page.fields_dict.department.$input.on('change', function() {
+	page.fields_dict.department.$input.on('change', function () {
 		if (!page.fields_dict.department.get_value()) {
-				refresh_projects(page);
+			refresh_projects(page);
 		}
-  });
+	});
 	let subcategoryField = page.add_field({
 		label: __("Compliance Sub Category"),
 		fieldname: "compliance_sub_category",
@@ -89,46 +89,46 @@ function make_filters(page) {
 		options: "Compliance Sub Category",
 		change() {
 			if (page.fields_dict.compliance_sub_category.get_value()) {
-          refresh_projects(page);
-      }
+				refresh_projects(page);
+			}
 		}
 	});
-	page.fields_dict.compliance_sub_category.$input.on('change', function() {
+	page.fields_dict.compliance_sub_category.$input.on('change', function () {
 		if (!page.fields_dict.compliance_sub_category.get_value()) {
-				refresh_projects(page);
+			refresh_projects(page);
 		}
-  });
+	});
 	let fromDateField = page.add_field({
 		label: __("From Date"),
 		fieldname: "from_date",
 		fieldtype: "Date"
 	});
-	page.fields_dict.from_date.$input.off('change').on('change', function() {
-      refresh_projects(page);
-  });
+	page.fields_dict.from_date.$input.off('change').on('change', function () {
+		refresh_projects(page);
+	});
 	let toDateField = page.add_field({
 		label: __("To Date"),
 		fieldname: "to_date",
 		fieldtype: "Date"
 	});
-	page.fields_dict.to_date.$input.off('change').on('change', function() {
-      refresh_projects(page);
-  });
+	page.fields_dict.to_date.$input.off('change').on('change', function () {
+		refresh_projects(page);
+	});
 	let status = page.add_field({
 		label: __("Status"),
 		fieldname: "status",
 		fieldtype: "Select",
 		options: [
-				{},
-				{ label: "Open", value: "open" },
-				{ label: "Invoiced", value: "invoiced" },
-				{ label: "Paid", value: "paid" },
-				{ label: "Overdue", value: "overdue" },
-				{ label: "Hold", value: "hold" },
-				{ label: "Completed", value: "completed" },
-				{ label: "Cancelled", value: "cancelled" },
-			],
-			default: "",
+			{},
+			{ label: "Open", value: "open" },
+			{ label: "Invoiced", value: "invoiced" },
+			{ label: "Paid", value: "paid" },
+			{ label: "Overdue", value: "overdue" },
+			{ label: "Hold", value: "hold" },
+			{ label: "Completed", value: "completed" },
+			{ label: "Cancelled", value: "cancelled" },
+		],
+		default: "",
 		change() {
 			refresh_projects(page);
 		}
@@ -136,17 +136,17 @@ function make_filters(page) {
 }
 
 function get_employee_id() {
-    if (frappe.session.user === 'Administrator') {
-        return '';
-    }
-		else {
-				return frappe.session.user
-		}
+	if (frappe.session.user === 'Administrator') {
+		return '';
+	}
+	else {
+		return frappe.session.user
+	}
 }
 
-function refresh_projects(page){
+function refresh_projects(page) {
 	// Clear existing projects from the page
-  page.body.find(".frappe-list").remove();
+	page.body.find(".frappe-list").remove();
 
 	const selectedStatus = page.fields_dict.status.get_value();
 	const projectName = page.fields_dict.project.get_value();
@@ -158,38 +158,39 @@ function refresh_projects(page){
 	const to_date = page.fields_dict.to_date.get_value();
 
 	frappe.call({
-			method: "one_compliance.one_compliance.page.project_management_tool.project_management_tool.get_project",
-			 args: { status: selectedStatus,
-							 project: projectName,
-							 customer: customerName,
-							 department: department,
-							 sub_category: subCategory,
-							 employee: employee,
-							 from_date: from_date,
-							 to_date: to_date
-							},
-			 callback: (r) => {
-				if (r.message && r.message.length>0) {
-						$(frappe.render_template("project_management_tool", {project_list:r.message})).appendTo(page.body);
+		method: "one_compliance.one_compliance.page.project_management_tool.project_management_tool.get_project",
+		args: {
+			status: selectedStatus,
+			project: projectName,
+			customer: customerName,
+			department: department,
+			sub_category: subCategory,
+			employee: employee,
+			from_date: from_date,
+			to_date: to_date
+		},
+		callback: (r) => {
+			if (r.message && r.message.length > 0) {
+				$(frappe.render_template("project_management_tool", { project_list: r.message })).appendTo(page.body);
 
-						// Action to redirect to the task management tool 
-						page.body.find(".showTask").on("click", function () {
-							var project_id = $(this).attr("project");
-							console.log(project_id);
-
-							// Store the selected project ID in localStorage
-					    localStorage.setItem('selected_project_id', project_id);
-
-					    // Navigate to the task management tool page
-					    window.location.href = '/app/task-management-tool';
-						});
-				} else {
-            // If no projects are found, append a message to the page body
-            $('<div class="frappe-list"></div>').appendTo(page.body).append('<div class="no-result text-muted flex justify-center align-center" style="text-align: center;"><p>No Project found with matching filters.</p></div>');
-        }
-			},
-			freeze: true,
-			freeze_message: 'Loading Project'
-		});
+				// Action to redirect to the task management tool 
+				page.body.find(".showTask").on("click", function () {
+					var project_id = $(this).attr("project");
+										
+					// Set route options before navigation
+					frappe.route_options = {
+						project: project_id
+					};					
+					// Navigate to the task management tool page
+					frappe.set_route('task-management-tool');
+				});
+			} else {
+				// If no projects are found, append a message to the page body
+				$('<div class="frappe-list"></div>').appendTo(page.body).append('<div class="no-result text-muted flex justify-center align-center" style="text-align: center;"><p>No Project found with matching filters.</p></div>');
+			}
+		},
+		freeze: true,
+		freeze_message: 'Loading Project'
+	});
 
 }
