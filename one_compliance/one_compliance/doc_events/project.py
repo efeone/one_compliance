fbf8879446431e07eb06532a634f2fe258e2ba92
Muhammed Sinan K T <91651425+MhmdSinanKT@users.noreply.github.com>
2025-11-07 14:31:10 +0530

    fix: missing import

--- one_compliance/one_compliance/doc_events/project.py
+++ one_compliance/one_compliance/doc_events/project.py
@@ -1,7 +1,7 @@
 import frappe
 from frappe import _
 from frappe.email.doctype.notification.notification import get_context
-from frappe.utils import add_days, getdate
+from frappe.utils import add_days, getdate, today
 from one_compliance.one_compliance.doc_events.task import (
 	create_sales_order,
 	get_rate_from_compliance_agreement,
@@ -104,7 +104,7 @@ def project_after_insert(doc, method):
 				frappe.db.set_value("Sales Order", sales_order, {
 					"status": "Proforma Invoice",
 					"workflow_state": "Proforma Invoice",
-					"invoice_generation_date": frappe.utils.today(),
+					"invoice_generation_date": today(),
 				})
 
 			else:
