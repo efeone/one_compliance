b108c494bd8b98f569375495c2003e0c7d876f90
Avishna Murali <avishnachinnu26@gmail.com>
2025-11-15 11:38:07 +0530

    feat:page-length button highlighting

--- one_compliance/one_compliance/page/project_management_tool/project_management_tool.js
+++ one_compliance/one_compliance/page/project_management_tool/project_management_tool.js
@@ -192,6 +192,10 @@ function render_pagination_controls(page, result_count) {
 Sets up event listeners for page length buttons to change the number of items displayed per page.
 */
 function setup_page_length_buttons(page) {
+	$(".page-length-btn").removeClass("active");
+
+	$(`.page-length-btn[data-length="${page.page_length}"]`).addClass("active");
+
 	$(".page-length-btn").off('click').on('click', function () {
 		$(".page-length-btn").removeClass("active");
 		$(this).addClass("active");
