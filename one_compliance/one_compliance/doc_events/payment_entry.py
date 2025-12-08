43ccd4108fb75fc1ff172c77aef440e4317f33f3
hridyalakshmi <hridyalkrishnan2001@gmail.com>
2025-12-08 11:08:33 +0530

    fix: connect so and project

--- one_compliance/one_compliance/doc_events/payment_entry.py
+++ one_compliance/one_compliance/doc_events/payment_entry.py
@@ -2,28 +2,34 @@ import frappe
 
 
 def payment_entry_on_submit(doc, method):
-	"""method sets workflow for Sales Order if the Payment Entry is referenced to an SI against it"""
+    """Sets workflow for Sales Order based on Sales Invoice updated status"""
 
-	for payment_reference in doc.references:
+    for ref in doc.references:
 
-		if payment_reference.reference_doctype == "Sales Invoice":
+        if ref.reference_doctype == "Sales Invoice":
 
-			sales_order = frappe.db.get_value(
-				"Sales Invoice Item",
-				{"parent": payment_reference.reference_name},
-				"sales_order",
-			)
+            si = frappe.get_doc("Sales Invoice", ref.reference_name)
 
-			if payment_reference.outstanding_amount == 0:
-				new_state = "Paid"
-				project_status = "Paid"
-			elif payment_reference.allocated_amount < payment_reference.outstanding_amount:
-					new_state = "Partially Paid"
-					project_status = "Partially Paid" 
-			else:
-				continue
-			
-			frappe.db.set_value("Sales Order", sales_order, "workflow_state", new_state)
-			project = frappe.db.get_value("Sales Order", sales_order, "project")
-			if project:
-				frappe.db.set_value("Project", project, "status", project_status)
+            sales_order = frappe.db.get_value(
+                "Sales Invoice Item",
+                {"parent": si.name},
+                "sales_order",
+            )
+
+            outstanding = si.outstanding_amount
+            grand_total = si.rounded_total or si.grand_total
+
+            if outstanding == 0:
+                new_state = "Paid"
+                project_status = "Paid"
+
+            elif outstanding < grand_total:
+                new_state = "Partially Paid"
+                project_status = "Partially Paid"
+
+            else:
+                continue
+            frappe.db.set_value("Sales Order", sales_order, "workflow_state", new_state)
+            project = frappe.db.get_value("Sales Order", sales_order, "project")
+            if project:
+                frappe.db.set_value("Project", project, "status", project_status)
