2f6583f05a793d868d6edd57a0431ddb86ad3db5
Muhammed Sinan K T <m.sinankt@efeone.com>
2026-01-09 13:06:01 +0530

    feat: outsourced project

--- one_compliance/custom/custom_field/sales_order.py
+++ one_compliance/custom/custom_field/sales_order.py
@@ -162,5 +162,51 @@ def get_sales_order_custom_fields():
 				"label": "Is Premium Project",
 				"insert_after": "amended_from"
 			},
+			{
+				"fieldname": "is_outsource_service",
+				"fieldtype": "Check",
+				"label": "Is Outsource Service",
+				"insert_after": "is_premium_project",
+				"allow_on_submit": 1
+			},
+			{
+				"fieldname": "supplier",
+				"fieldtype": "Link",
+				"label": "Supplier",
+				"options": "Supplier",
+				"insert_after": "custom_billing_date",
+				"allow_on_submit": 1,
+				"depends_on": "eval: doc.is_outsource_service",
+				"mandatory_depends_on": "eval: doc.is_outsource_service"
+			},
+			{
+				"fieldname": "purchase_invoice",
+				"fieldtype": "Link",
+				"label": "Purchase Invoice",
+				"options": "Purchase Invoice",
+				"insert_after": "supplier",
+				"allow_on_submit": 1,
+				"depends_on": "eval: doc.is_outsource_service"
+			},
+			{
+				"fieldname": "payment_status",
+				"fieldtype": "Data",
+				"label": "Payment Status",
+				"insert_after": "purchase_invoice",
+				"read_only": 1,
+				"depends_on": "eval: doc.is_outsource_service",
+				"fetch_from": "purchase_invoice.status",
+				"allow_on_submit": 1,
+			},
+			{
+				"fieldname": "outstanding_amount",
+				"fieldtype": "Currency",
+				"label": "Outstanding Amount",
+				"fetch_from": "purchase_invoice.outstanding_amount",
+				"read_only": 1,
+				"depends_on": "eval: doc.is_outsource_service",
+				"insert_after": "payment_status",
+				"allow_on_submit": 1,
+    		}
 		]
 	}
