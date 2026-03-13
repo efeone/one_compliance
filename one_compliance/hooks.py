2fe6affb8b0fdc1fdb513bc7e32c3b1c038d02e7
hridyalakshmi <hridyalkrishnan2001@gmail.com>
2026-03-13 15:43:38 +0530

    feat: Set Invoice generation date in Sales order when the workflow state changes to Proforma Invoice

--- one_compliance/hooks.py
+++ one_compliance/hooks.py
@@ -171,7 +171,10 @@ doc_events = {
 	'Sales Order':{
 		'on_submit':'one_compliance.one_compliance.doc_events.sales_order.create_project_on_submit',
 		'on_cancel': 'one_compliance.one_compliance.doc_events.sales_order.so_on_cancel_custom',
-		'on_update_after_submit': 'one_compliance.one_compliance.doc_events.sales_order.so_on_update_after_submit',
+		'on_update_after_submit': [
+			'one_compliance.one_compliance.doc_events.sales_order.so_on_update_after_submit',
+			'one_compliance.one_compliance.doc_events.sales_order.set_invoice_generation_date'
+		],
 		'validate': 'one_compliance.one_compliance.doc_events.sales_order.set_compliance_fields'
 	},
 	'Payment Entry':{
